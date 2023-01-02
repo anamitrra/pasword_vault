3a6a4dba9fbe1be543e06e480c1449f5e581a948
anamitrra <32793691+anamitrra@users.noreply.github.com>
2023-01-02 16:20:37 +0530

    done

--- server/index.js
+++ server/index.js
@@ -1,23 +1,26 @@
 const express = require('express');
 const mysql = require('mysql');
-const cors = require(`cors`);
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
 const app = express();
 const PORT = 3001;
 
 
 const db = mysql.createConnection({
     user: "root",
-    host: "localhost",
+    host: "localhost:3306",
     password: "",
     database: "passwordmanager",
 });
-
 app.use(cors());
+// app.use(express.json());
+// app.use(bodyParser.json()) // for parsing application/json
+// app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.json());
+app.use('/api', Anyroute)
 
-
-
-app.post("/addpassword", (res, req) => {
+app.post(`/addpassword`, (res, req) => {
     const { password, description } = req.body;
     db.query(`INSERT INTO passwords(password, description) VALUES (${password},${description})`,
         (err, result) => {
